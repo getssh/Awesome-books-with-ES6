621d1479c62e170565f767b4e260ba6c95771597
getssh <gtamrat33@gmail.com>
2023-02-21 21:43:27 +0300

    Add localstorage functionality to books array

--- index.js
+++ index.js
@@ -2,54 +2,53 @@ const bookList = document.querySelector('.books');
 const bookTitle = document.querySelector('#title');
 const bookAuthor = document.querySelector('#author');
 const addBtn = document.querySelector('.submit-btn');
-const showListSection = document.querySelector('.show-list');
+// const showListSection = document.querySelector('.show-list');
 const book = document.querySelector('.book');
-let books = [];
+let books = localStorage.books
+  ? JSON.parse(localStorage.getItem('books')) : [];
 
-let allBooks;
 const showList = () => {
-  let bookContent = ``;
+  let bookContent = '';
   for (let i = 0; i < books.length; i += 1) {
-    let tempBook = `
+    const tempBook = `
     <p class="book-title">${books[i].title}</p>
     <p>by</p>
     <p class="book-author">${books[i].author}</p>
     <button id=${books[i].id} onclick="removeBook(${books[i].id})">Remove</button>
-    `
+    `;
     bookContent += tempBook;
   }
 
   book.innerHTML = bookContent;
   bookList.appendChild(book);
-}
+};
 
 window.addEventListener('load', showList);
 
 const addBook = (title, author) => {
-  let pushBook = {
+  const pushBook = {
     id: Math.floor(Math.random() * Date.now()),
-    title: title,
-    author: author
-  }
+    title,
+    author,
+  };
   books.push(pushBook);
   bookTitle.value = '';
   bookAuthor.value = '';
   showList();
-}
+  localStorage.setItem('books', JSON.stringify(books));
+};
 
 addBtn.addEventListener('click', (event) => {
   event.preventDefault();
   addBook(bookTitle.value, bookAuthor.value);
-})
+});
 
 const removeBook = (id) => {
-  let filterBook = books.filter((book) => {
-    return book.id !== id;
-  })
+  const filterBook = books.filter((book) => book.id !== id);
 
   books = filterBook;
-  console.log(books);
-  showList()
-}
+  showList();
+  localStorage.setItem('books', JSON.stringify(books));
+};
 
-console.log(books);
+console.log(removeBook());
