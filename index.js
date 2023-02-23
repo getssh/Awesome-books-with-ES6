cc7b2dbcdbd481c903dbb317eb4349092b24ad2b
getssh <gtamrat33@gmail.com>
2023-02-23 13:21:05 +0300

    Add eventListeners for diffrent tabs to render daynamiclally

--- index.js
+++ index.js
@@ -9,3 +9,31 @@ addButton.addEventListener('click', (event) => {
 });
 
 bookList.render();
+
+const list = document.querySelector('.list');
+const addNew = document.querySelector('.add-new');
+const contact = document.querySelector('.contact');
+const showList  = document.querySelector('.show-list');
+const showAdd = document.querySelector('.show-new');
+const showContact = document.querySelector('.show-contact');
+
+list.addEventListener('click', (e) => {
+  e.preventDefault();
+  showList.style.display = "block";
+  showAdd.style.display = "none";
+  showContact.style.display = "none";
+})
+
+addNew.addEventListener('click', (e) => {
+  e.preventDefault();
+  showList.style.display = "none";
+  showAdd.style.display = "block";
+  showContact.style.display = "none";
+})
+
+contact.addEventListener('click', (e) => {
+  e.preventDefault();
+  showList.style.display = "none";
+  showAdd.style.display = "none";
+  showContact.style.display = "block";
+})
